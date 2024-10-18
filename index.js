1209617805816acef630f2ef193dcaf8f8ad4f34
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-18 17:09:48 -0300

    add função para determinar o nível de acordo com o saldo de vitórias

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ function determinarSaldoVitorias(vitorias, derrotas){
     let saldoVitorias = vitorias - derrotas;
     return saldoVitorias;
 }
+
+function determinarNivel(saldoVitorias){
+    let nivel;
+
+    if(saldoVitorias < 10)
+        nivel = "Ferro";
+    else if(saldoVitorias <= 20)
+        nivel = "Bronze";
+    else if(saldoVitorias <= 50)
+        nivel = "Prata";
+    else if(saldoVitorias <= 80)
+        nivel = "Ouro";
+    else if(saldoVitorias <= 90)
+        nivel = "Diamante";
+    else if(saldoVitorias <= 100)
+        nivel = "Lendário";
+    else
+        nivel = "Imortal";
+
+    return nivel;
+}
